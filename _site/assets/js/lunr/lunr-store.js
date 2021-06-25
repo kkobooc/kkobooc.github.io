db929379f70ccba8fe55cdff541e48388a03bfcd
kkobooc@gmail.com <kkobooc@gmail.com>
2021-06-25 13:45:12 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"Github Pages를 이용해 첫 블로그 생성!   앞으로 열심히 써야지..   YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다.   이 글의 제목은 github.io 블로그 시작하기이고   마지막으로 수정된 시간은 2021-06-25이다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
