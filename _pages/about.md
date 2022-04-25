233ee8e070ed13c9b4ac8eeeb51e22cb04a1901d
Jaecheol Choi <jc@Jaecheolui-MacBookAir.local>
2022-04-25 19:04:11 +0900

    basic settings

---
+++ _pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About"
+permalink: /about/
+layout: single
+---
+
+## kkobooc.github.io 블로그
+
+Data, 스포츠를 좋아합니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,5 +3,5 @@ var store = [{
         "excerpt":"Github Pages를 이용해 첫 블로그 생성!   이 글의 제목은 첫 테스트 게시물이고   마지막으로 수정된 시간은 2021-06-25이다.  ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "/blog/test/",
-        "teaser": null
+        "teaser": "/assets/images/kkobooc.png"
       }]
